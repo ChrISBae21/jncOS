a00512f1714f48f4f82f5ffe54ed571b15dd6dfa
ChrISBae21 <cbgnote@gmail.com>
2025-04-02 15:44:04 -0700

    timer with no interrupts working

--- testing/src/kernel.c
+++ testing/src/kernel.c
@@ -3,9 +3,11 @@
 #include "uart.h"
 #include "printf.h"
 #include "util.h"
+#include "timer.h"
 
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
     /* cast to void types */
+
     (void)r0;
     (void)r1;
     (void)atags;
@@ -14,21 +16,14 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
     init_uart();
     print_intro();
     init_printf(0, putc);
+    timer_init();
+
     int el = get_el();
 	printf("Exception level: %d \r\n", el);
 
     /* print input */
     while (1) {
-        c = uart_getc();
-        if (c == '\r') {
-            uart_putc('\n');
-        }
-        else if (c == 127) {
-            uart_print("\b \b");
-        }
-        else {
-            uart_putc(c);
-        }
+        while(!(get32(TIMER_CS) & TIMER_CS_M1));
+        handle_timer_irq();
     }
-
 }
