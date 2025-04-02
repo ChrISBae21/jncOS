4b3217e27d48d888216075e97ec688cec8d35153
ChrISBae21 <cbgnote@gmail.com>
2025-04-02 12:50:18 -0700

    Creating branch for EL1

--- testing/src/kernel.c
+++ testing/src/kernel.c
@@ -3,52 +3,33 @@
 #include "uart.h"
 #include "printf.h"
 #include "util.h"
-#include "irq.h"
-#include "timer.h"
-// #include <limits.h>
 
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
     /* cast to void types */
     (void)r0;
     (void)r1;
     (void)atags;
-    // unsigned char c;
+    unsigned char c;
 
-    // init_uart();
-    // print_intro();
-    // init_printf(0, putc);
-    // int el = get_el();
-	// printf("Exception level: %d \r\n", el);
-    // irq_vector_init();
-	// timer_init();
-	// enable_interrupt_controller();
-	// enable_irq();
+    init_uart();
+    print_intro();
+    init_printf(0, putc);
+    int el = get_el();
+	printf("Exception level: %d \r\n", el);
+    // testPrint();
 
-    // // testPrint();
-
-    // /* print input */
-    // while (1) {
-    //     c = uart_getc();
-    //     if (c == '\r') {
-    //         uart_putc('\n');
-    //     }
-    //     else if (c == 127) {
-    //         uart_print("\b \b");
-    //     }
-    //     else {
-    //         uart_putc(c);
-    //     }
-    // }
-
-	init_uart();
-	init_printf(0, putc);
-	irq_vector_init();
-	timer_init();
-	enable_interrupt_controller();
-	enable_irq();
-
-	while (1){
-		uart_putc(uart_getc());
-	}	
+    /* print input */
+    while (1) {
+        c = uart_getc();
+        if (c == '\r') {
+            uart_putc('\n');
+        }
+        else if (c == 127) {
+            uart_print("\b \b");
+        }
+        else {
+            uart_putc(c);
+        }
+    }
 
 }
